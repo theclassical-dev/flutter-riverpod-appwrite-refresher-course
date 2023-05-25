53d4aa6194edfc26d84b9406362e861ecb6b3229
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2023-05-25 08:57:32 +0100

    ui_constants added + login stil under designning

---
+++ lib/constants/ui_constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:riverpod_twitter_course/theme/theme.dart';
+
+import 'constants.dart';
+
+class UIConstants {
+  static AppBar appBar() {
+    return AppBar(
+      title: SvgPicture.asset(
+        AssetsConstants.twitterLogo,
+        color: Pallete.blueColor,
+        height: 30,
+      ),
+      centerTitle: true,
+    );
+  }
+}
