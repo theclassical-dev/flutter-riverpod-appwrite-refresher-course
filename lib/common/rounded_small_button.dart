449a961de49e1b63a503dc2395bd4361e8890f23
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2023-05-25 09:49:31 +0100

    login screen designed + auth_field and rounded_small_button added

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_twitter_course/theme/theme.dart';
+
+class RoundSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+
+  const RoundSmallButton({
+    super.key,
+    required this.onTap,
+    required this.label,
+    this.backgroundColor = Pallete.whiteColor,
+    this.textColor = Pallete.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(
+        label,
+        style: TextStyle(color: textColor, fontSize: 16),
+      ),
+      backgroundColor: backgroundColor,
+      labelPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+    );
+  }
+}
--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:riverpod_twitter_course/common/rounded_small_button.dart';
 import 'package:riverpod_twitter_course/constants/ui_constants.dart';
+import 'package:riverpod_twitter_course/features/auth/widgets/auth_field.dart';
+import 'package:riverpod_twitter_course/theme/theme.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,11 +14,78 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   final appbar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(children: [
+              //textfeild
+              AuthField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //textfeild password
+              AuthField(
+                controller: passwordController,
+                hintText: 'Password',
+              ),
+
+              const SizedBox(
+                height: 40,
+              ),
+              //button
+              Align(
+                alignment: Alignment.topRight,
+                child: RoundSmallButton(
+                  onTap: () {},
+                  label: 'Done',
+                ),
+              ),
+
+              const SizedBox(
+                height: 40,
+              ),
+
+              //text
+              RichText(
+                  text: TextSpan(
+                      text: "Dont have an account?",
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      children: [
+                    TextSpan(
+                      text: ' Sign Up',
+                      style: const TextStyle(
+                        color: Pallete.blueColor,
+                        fontSize: 16,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                  ]))
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
